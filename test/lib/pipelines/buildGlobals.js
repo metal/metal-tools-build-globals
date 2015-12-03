e827fccf17c063170670c3320a619688df70ac19
Maira Bello <maira.araujo@liferay.com>
2015-12-03 16:31:45 -0300

    Adds test for the buildGlobals pipeline

--- test/lib/pipelines/buildGlobals.js
+++ test/lib/pipelines/buildGlobals.js
@@ -2,9 +2,23 @@
 
 var assert = require('assert');
 var buildGlobals = require('../../../lib/pipelines/buildGlobals');
+var consume = require('stream-consume');
 var vfs = require('vinyl-fs');
 
 describe('Pipeline - Build to globals', function() {
-	it('should build js files to a globals bundle', function(done) {
+	it('should build js files to a globals bundle and its source map', function(done) {
+		var stream = vfs.src('test/fixtures/js/foo.js')
+      .pipe(buildGlobals());
+
+		var files = [];
+    stream.on('data', function(file) {
+			files.push(file.relative);
+		});
+		stream.on('end', function() {
+			assert.strictEqual(2, files.length);
+			assert.deepEqual(['metal.js', 'metal.js.map'], files.sort());
+			done();
+		});
+		consume(stream);
 	});
 });
