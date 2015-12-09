c96ff6877e30da3793df99792e5f6819d0875c84
Maira Bello <maira.araujo@liferay.com>
2015-12-09 14:03:56 -0300

    Adds option to not consume stream automatically

--- test/index.js
+++ test/index.js
@@ -8,39 +8,51 @@ var sinon = require('sinon');
 var vfs = require('vinyl-fs');
 
 describe('Metal Tools - Build Globals', function() {
-  describe('Default src/dest', function() {
-    beforeEach(function() {
-      var pipe = {
-        pipe: function() {
-          return pipe;
-        }
-      };
-      sinon.stub(vfs, 'src').returns(pipe);
-      sinon.stub(vfs, 'dest');
-    });
+  beforeEach(function() {
+    var stream = {
+      pipe: function() {
+        return stream;
+      },
+      readable: true,
+      resume: sinon.stub()
+    };
+    sinon.stub(vfs, 'src').returns(stream);
+    sinon.stub(vfs, 'dest');
+  });
 
-    afterEach(function() {
-      vfs.src.restore();
-      vfs.dest.restore();
-    });
+  afterEach(function() {
+    restoreStream();
+  });
 
-    it('should compile soy files from "src" and to "build/globals" by default', function() {
-      metalToolsBuildGlobals();
-      assert.strictEqual('src/**/*.js', vfs.src.args[0][0]);
-      assert.strictEqual('build/globals', vfs.dest.args[0][0]);
-  	});
+  it('should build files from "src" and to "build/globals" by default', function() {
+    metalToolsBuildGlobals();
+    assert.strictEqual('src/**/*.js', vfs.src.args[0][0]);
+    assert.strictEqual('build/globals', vfs.dest.args[0][0]);
+	});
+
+  it('should consume stream by default', function() {
+    var stream = metalToolsBuildGlobals();
+    assert.strictEqual(1, stream.resume.callCount);
+  });
+
+  it('should not consume stream if skipConsume is set to true', function() {
+    var stream = metalToolsBuildGlobals({
+      skipConsume: true
+    });
+    assert.strictEqual(0, stream.resume.callCount);
   });
 
   describe('Integration', function() {
     beforeEach(function(done) {
       deleteBuiltFiles(done);
+      restoreStream();
     });
 
   	after(function(done) {
       deleteBuiltFiles(done);
   	});
 
-  	it('should compile specified soy files to js', function(done) {
+  	it('should build specified js files into a single bundle', function(done) {
       var stream = metalToolsBuildGlobals({
         src: 'test/fixtures/js/foo.js',
         dest: 'test/fixtures/build'
@@ -57,4 +69,13 @@ function deleteBuiltFiles(done) {
   del('test/fixtures/build').then(function() {
     done();
   });
+}
+
+function restoreStream() {
+  if (vfs.src.restore) {
+    vfs.src.restore();
+  }
+  if (vfs.dest.restore) {
+    vfs.dest.restore();
+  }
 }
